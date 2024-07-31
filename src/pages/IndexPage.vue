85498713a3039f689aca754f7526d8a718e3ba47
pexrapat <peerapatwasdzx@gmail.com>
2024-07-31 11:15:14 +0700

    Modified reset method

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -93,7 +93,9 @@ export default {
       },
 
       onReset () {
+        id.value = null
         name.value = null
+        surname.value = null
         age.value = null
         accept.value = false
       }
